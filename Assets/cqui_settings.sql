c0b3770ef3550cfc14602e021013ccb2400ad856
chaorace <chaorace@gmail.com>
2017-04-10 20:55:59 -0400

    Fixed setting name/category issue

--- Assets/cqui_settings.sql
+++ Assets/cqui_settings.sql
@@ -36,6 +36,7 @@ INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
       ("CQUI_AutoapplyBuilderLens", 1), -- Automatically activates the builder lens when selecting a builder
       ("CQUI_AutoapplyScoutLens", 1), -- Automatically activates the scout lens when selecting a scout
       ("CQUI_AutoExpandUnitActions", 1), -- Automatically reveals the secondary unit actions normally hidden inside an expando
+      ("CQUI_BlockOnCityAttack", 1), -- Block turn from ending if you have a city that can attack
       ("CQUI_ProductionQueue", 1), -- A production queue appears next to the production panel, allowing multiple constructions to be queued at once
       ("CQUI_ShowCultureGrowth", 1), -- Shows cultural growth overlay in cityview
       ("CQUI_ShowLuxuries", 1), -- Luxury resources will show in the top-bar next to strategic resources
@@ -49,8 +50,7 @@ INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
       ("CQUI_SmartWorkIcon", 1), -- Applies a different size/transparency to citizen icons if they're currently being worked
       ("CQUI_TechPopupVisual", 0), -- Popups will be displayed when you discover a new tech or civic (this is the normal behavior for the unmoded game)
       ("CQUI_TechPopupAudio", 1), -- Play the voiceovers when you discover a new tech or civic (this is the normal behavior for the unmoded game)
-      ("CQUI_ToggleYieldsOnLoad", 1), -- Toggles yields immediately on load
-      ("CQUI_BlockOnCityAttack", 1); -- Block turn from ending if you have a city that can attack
+      ("CQUI_ToggleYieldsOnLoad", 1); -- Toggles yields immediately on load
 
 /*
     ┌────────────────────────────────────────────────────────────────────────────────────────────┐
